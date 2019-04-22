1cebb4e8c5d1e80658d9c688603fa808656f89da
Bhavyam Goyal <bhavyam@outscal.com>
2019-04-22 18:37:16 +0530

    setup for asset store

---
+++ Assets/scripts/testing.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using IL_TabView;
+
+public class testing :ScrollSnapRect{
+	public override void IL_Start () {
+		
+	}
+	
+	public override void IL_Update () {
+		
+	}
+}
